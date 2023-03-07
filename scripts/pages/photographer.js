85f45ceeb54feec851787cb8b77ae3bacf9a2efb
Pierre <pierrelouis.abraham@gmail.com>
2023-03-07 17:57:51 +0100

    test

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -168,11 +168,11 @@ function nextImage () {
     }
     if('video' in currentMedias[nextIndex]) {
         openModalLightbox("assets/Sample Photos/" + currentImageSrc + '/' + (currentMedias[nextIndex].video), currentMedias[nextIndex].id, currentMedias[nextIndex].title); 
-        console.log(currentImageSrc)
+        console.log(currentImageSrc, currentImage)
     }
     else {
         openModalLightbox("assets/Sample Photos/" + currentImageSrc + '/' + (currentMedias[nextIndex].image), currentMedias[nextIndex].id, currentMedias[nextIndex].title);
-        console.log(currentImageSrc)
+        console.log(currentImageSrc, currentImage)
     }
 }
     
