ed79ff322d989cbc41e971a4edfc8e72d3c41d25
Pierre <pierrelouis.abraham@gmail.com>
2023-03-07 17:31:14 +0100

    test changes for pages

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -158,7 +158,7 @@ return sum;
 
 function nextImage () {
     const currentImage = document.getElementById("image_lightbox")
-    const currentImageSrc = currentImage.src.split('/')[5];
+    const currentImageSrc = currentImage.src.split('/')[6];
     const currentImageName = currentImage.getAttribute("name");
     const compare = (element) => element.id == currentImageName;
     var index = currentMedias.findIndex(compare);
